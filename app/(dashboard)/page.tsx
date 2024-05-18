3335588881d3023bafc1d57d729bf135df096de7
Akumuuu <88854566+Akumuuu@users.noreply.github.com>
2024-05-18 15:51:08 +0200

    collection

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,20 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Skeleton } from '@/components/ui/skeleton';
+import prisma from '@/lib/prisma';
 import { wait } from '@/lib/wait';
 import { currentUser } from '@clerk/nextjs/server';
 import { Suspense } from 'react';
+import SadFace from '@/components/icons/SadFace';
+import CreateCollectionBtn from '@/components/CreateCollectionBtn';
 
 export default async function Home() {
   return (
     <>
       <Suspense fallback={<WelcomeMsgFallback />}>
         <WelcomeMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading collections...</div>}>
+        <CollectionList />
       </Suspense>
     </>
   );
@@ -21,7 +28,7 @@ async function WelcomeMsg() {
   }
 
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <h1 className="text-4xl font-bold">
         Welcome, <br /> {user.firstName} {user.lastName}
       </h1>
@@ -31,11 +38,35 @@ async function WelcomeMsg() {
 
 function WelcomeMsgFallback() {
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <h1 className="text-4xl font-bold">
         <Skeleton className="w-[150px] h-[36px]" />
         <Skeleton className="w-[150px] h-[36px]" />
       </h1>
     </div>
   );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
 }
---
+++ components/CreateCollectionSheet.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Sheet, SheetContent, SheetDescription, SheetTitle } from './ui/sheet';
+
+interface Props {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+function CreateCollectionSheet({ open, onOpenChange }: Props) {
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <SheetContent>
+        <SheetTitle>Add new collection</SheetTitle>
+        <SheetDescription>
+          Collections are a way to group your tasks
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default CreateCollectionSheet;
